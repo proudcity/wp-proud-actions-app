c320257b7ff1155b051f63fefca09711965141f1
Jeff Lyon <jeff@albatrossdigital.com>
2016-09-30 15:20:41 -0700

    Fixing how we get mapbox settings

--- includes/js/proud-actions-app/app/js/apps/311App/311App.js
+++ includes/js/proud-actions-app/app/js/apps/311App/311App.js
@@ -32,10 +32,8 @@ angular.module('311App', [
       $rootScope.activeCity = _.get(Proud, 'settings.global.location.city') || 'Huntsville';
       $rootScope.activeState = _.get(Proud, 'settings.global.location.state') || 'Alabama';
       $rootScope.location = _.get(Proud, 'settings.global.location') || {lat: 0, lng: 0};
-      // $rootScope.mapboxAccessToken = _.get(Proud, 'settings.global.mapbox.token') || 'pk.eyJ1IjoicHJvdWRjaXR5IiwiYSI6ImNpa2JsN2h4eTBtM3N2NGt1ZWltdmRwM3gifQ.yMFhYzprJEYIecChP-rJAg';
-      // $rootScope.mapboxMap = _.get(Proud, 'settings.global.mapbox.map') || 'https://api.mapbox.com/v3/mapbox.streets.json';
-      $rootScope.mapboxAccessToken = 'pk.eyJ1IjoiYWxiYXRyb3NzZGlnaXRhbCIsImEiOiI1cVUxbUxVIn0.SqKOVeohLfY0vfShalVDUw';
-      $rootScope.mapboxMap = 'albatrossdigital.lpkdpcjb';
+      $rootScope.mapboxAccessToken = _.get(Proud, 'settings.global.mapbox.token') || '';
+      $rootScope.mapboxMap = _.get(Proud, 'settings.global.mapbox.map') || '';
 
       // Helper function returns boolean for if we're in "full page mode"
       $rootScope.displayExpanded = function(section) {
