f6e8cbc11e4174b2d30c8b50985ae828a4818cce
Jeff Lyon <jeff@albatrossdigital.com>
2016-11-02 17:13:59 -0700

    Debugging gravityforms iframe https://github.com/proudcity/wp-proudcity/issues/640

--- templates/gravityforms-iframe.php
+++ templates/gravityforms-iframe.php
@@ -4,6 +4,29 @@ require_once('../../../../wp-load.php');
 do_action('wp_enqueue_scripts');
 wp_deregister_script( 'angular' );
 
+//if ( empty( $wp->query_vars['gfiframe'] ) || ( 'gfembed' != $wp->query_vars['gfiframe'] && 'gfembed.php' != $wp->query_vars['gfiframe'] ) ) {
+//  return;
+//}
+
+$form_id = null;
+if ( ! empty( $_GET['id'] ) ) {
+  $form_id = absint( $_GET['id'] );
+} else {
+  // The request needs an 'f' query arg with the form id.
+  wp_die( esc_html__( 'Invalid form id.', 'gravity-forms-iframe' ) );
+}
+global $wp;
+
+
+$form = GFFormsModel::get_form_meta( $form_id );
+//$settings = $this->addon->get_form_settings( $form );
+
+// Disable the toolbar in case the form is embedded on the same domain.
+show_admin_bar( false );
+
+require_once( GFCommon::get_base_path() . '/form_display.php' );
+
+
 ?>
 
 <!doctype html>
@@ -16,7 +39,7 @@ wp_deregister_script( 'angular' );
   </head>
   <body>
 
-    <?php gravity_form( $_GET['id'], false, false, false, false, true ); ?>
-
+    <?php gravity_form( $form_id, false, false, false, false, true ); ?>
+    <?php wp_footer(); ?>
   </body>
 </html>
