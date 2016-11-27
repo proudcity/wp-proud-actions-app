f2810d7cdcdc0f4c47b6c24a46372a1a3ade047e
Jeff Lyon <jeff@albatrossdigital.com>
2016-11-27 14:29:00 -0800

    Adding Local Services settings https://github.com/proudcity/wp-proudcity/issues/616

--- settings/settings.php
+++ settings/settings.php
@@ -33,7 +33,7 @@ class ServiceCenterSettingsPage
 
     private function build_fields(  ) {
       $geojson_url = 'http://geojson.io/#map=14/'. get_option('lat', '0') .'/'. get_option('lng', '0');
-      $services_local = array(
+      /*$services_local = array(
         'garbage' => 'Garbage',
         'recycling' => 'Recycling',
         'police' => 'Police Department',
@@ -53,12 +53,12 @@ class ServiceCenterSettingsPage
       ];
       foreach ($services_local as $key => $value) {
         // @todo: make this work
-        /*$services_local_fields[$key] = [
+        $services_local_fields[$key] = [
           '#type' => 'select_media',
           '#title' => __pcHelp("$value GeoJSON file"),
           '#value' => $service_local_value[$key],
-        ];*/
-      }
+        ];
+      }*/
       $services_311 = array(
         'seeclickfix' => __pcHelp( 'SeeClickFix' ),
         'link' => __pcHelp( 'Link out to other provider' ),
@@ -95,54 +95,149 @@ class ServiceCenterSettingsPage
             ],
           ],
         ],
-      ] + $services_local_fields + [
-        // @todo
-        /*'service_map_layers' => [
-          '#type' => 'checkboxes',
-          '#title' => __pcHelp('Service Map Layers'),
-          '#name' => 'service_map_layers',
-          '#basic_array' => true,
-          '#options' => $this->map_layer_options(),
-          '#value' => $this->map_layer_select( get_option('service_map_layers', $this->map_layer_built_in() )),
-        ],*/
-        '311_service' => [
-          '#type' => 'radios',
-          '#title' => __pcHelp('Issues (311) provider'),
-          '#name' => '311_service',
-          '#options' => $services_311,
-          '#value' => get_option('311_service', 'seeclickfix')
-        ],
-        '311_link_create' => [
-          '#type' => 'text',
-          '#title' => __pcHelp('Create issue URL'),
-          '#value' => get_option('311_link_create'),
-          '#name' => '311_link_create',
-          '#states' => [
-            'visible' => [
-              '311_service' => [
-                'operator' => '==',
-                'value' => ['link'],
-                'glue' => '||'
+        'services_local' => [
+          '#title' => __( 'Local Services', 'wp-proud-core' ),
+          '#type' => 'group',
+          '#group_title_field' => 'title',
+          '#sub_items_template' => [
+            'title' => [
+              '#title' => 'Section title',
+              '#type' => 'text',
+              '#default_value' => '',
+              '#to_js_settings' => false
+            ],
+            'icon' => [
+              '#title' => 'Icon',
+              '#type' => 'fontawesome_select',
+              '#default_value' => '',
+              '#to_js_settings' => false
+            ],
+            'type' => [
+              '#title' => 'File type',
+              '#type' => 'radios',
+              '#default_value' => 'gis',
+              '#options' => array(
+                'hours' => 'Hours',
+                'gis' => 'GeoJSON GIS file',
+                'csv' => 'CSV (Comma Separated Values) file',
+              ),
+              '#description' => __pcHelp('For each Local Service, you will need to upload a CSV (Comma Separated Values) or <a href="'. $geojson_url .'" target="_blank">GeoJSON GIS</a> file and upload it below. <a href="https://proudcity.com/support/create" target="_blank">Contact ProudCity support</a> for assistance configuring your Local Services.')
+            ],
+            'hours' => [
+              '#title' => 'Hours',
+              '#type' => 'radios',
+              '#default_value' => 'gis',
+              '#options' => array(
+                'gis' => 'GeoJSON GIS file',
+                'csv' => 'CSV (Comma Separated Values) file',
+              ),
+              '#description' => __pcHelp('For each Local Service, you will need to upload a CSV (Comma Separated Values) or <a href="'. $geojson_url .'" target="_blank">GeoJSON GIS</a> file and upload it below. <a href="https://proudcity.com/support/create" target="_blank">Contact ProudCity support</a> for assistance configuring your Local Services.'),
+              '#states' => [
+                'visible' => [
+                  'type' => [
+                    'operator' => '==',
+                    'value' => ['hours'],
+                    'glue' => '||'
+                  ],
+                ],
+              ],
+            ],
+            'file' => [
+              '#title' => __( 'File', 'wp-proud-core' ),
+              '#type' => 'select_media',
+              '#default_value'  => '',
+              '#states' => [
+                'visible' => [
+                  'type' => [
+                    'operator' => '==',
+                    'value' => ['gis', 'csv'],
+                    'glue' => '||'
+                  ],
+                ],
+              ],
+            ],
+            'alert' => [
+              '#title' => 'Status alert notice',
+              '#type' => 'text',
+              '#default_value' => '',
+              '#description' => 'This could include information about service interruptions due to holidays, or other similar temporary service status alert notices.',
+              '#to_js_settings' => false
+            ],
+          ],
+          'services_hours' => [
+            '#title' => __( 'Now Services', 'wp-proud-core' ),
+            '#type' => 'group',
+            '#group_title_field' => 'title',
+            '#sub_items_template' => [
+              'title' => [
+                '#title' => 'Section title',
+                '#type' => 'text',
+                '#default_value' => '',
+                '#to_js_settings' => false
+              ],
+              'icon' => [
+                '#title' => 'Icon',
+                '#type' => 'fontawesome_select',
+                '#default_value' => '',
+                '#to_js_settings' => false
+              ],
+              
+              'alert' => [
+                '#title' => 'Status alert notice',
+                '#type' => 'text',
+                '#default_value' => '',
+                '#description' => 'This could include information about service interruptions due to holidays, or other similar temporary service status alert notices.',
+                '#to_js_settings' => false
+              ],
+            ],
+          ],
+          // @todo
+          /*'service_map_layers' => [
+            '#type' => 'checkboxes',
+            '#title' => __pcHelp('Service Map Layers'),
+            '#name' => 'service_map_layers',
+            '#basic_array' => true,
+            '#options' => $this->map_layer_options(),
+            '#value' => $this->map_layer_select( get_option('service_map_layers', $this->map_layer_built_in() )),
+          ],*/
+          '311_service' => [
+            '#type' => 'radios',
+            '#title' => __pcHelp('Issues (311) provider'),
+            '#name' => '311_service',
+            '#options' => $services_311,
+            '#value' => get_option('311_service', 'seeclickfix')
+          ],
+          '311_link_create' => [
+            '#type' => 'text',
+            '#title' => __pcHelp('Create issue URL'),
+            '#value' => get_option('311_link_create'),
+            '#name' => '311_link_create',
+            '#states' => [
+              'visible' => [
+                '311_service' => [
+                  'operator' => '==',
+                  'value' => ['link'],
+                  'glue' => '||'
+                ],
+              ],
+            ],
+          ],
+          '311_link_status' => [
+            '#type' => 'text',
+            '#title' => __pcHelp('Lookup issue URL'),
+            '#value' => get_option('311_link_status'),
+            '#name' => '311_link_status',
+            '#states' => [
+              'visible' => [
+                '311_service' => [
+                  'operator' => '==',
+                  'value' => ['link'],
+                  'glue' => '||'
+                ],
               ],
             ],
           ],
         ],
-        '311_link_status' => [
-          '#type' => 'text',
-          '#title' => __pcHelp('Lookup issue URL'),
-          '#value' => get_option('311_link_status'),
-          '#name' => '311_link_status',
-          '#states' => [
-            'visible' => [
-              '311_service' => [
-                'operator' => '==',
-                'value' => ['link'],
-                'glue' => '||'
-              ],
-            ],
-          ],
-        ],
-
       ];
     }
 
