d97dfca057a4c218ce15910c8c22ae7988c60608
Jeff Lyon <jeff@albatrossdigital.com>
2016-12-14 16:40:54 -0800

    Service center: path has two slashes after the tld https://github.com/proudcity/wp-proudcity/issues/738

--- lib/proud-actions-widget.class.php
+++ lib/proud-actions-widget.class.php
@@ -33,9 +33,9 @@ class ActionsBox extends Core\ProudWidget {
     // Running script
     wp_enqueue_script('proud-actions-app', $local_path . '/proud-actions-app.js', array('lodash','angular'), false, true);
     // Angular resources
-    wp_enqueue_script('proud-actions-app-libraries', $path . '/js/libraries.min.js', array('angular'), false, true);
-    wp_enqueue_script('proud-actions-app-app', $path . '/js/app.min.js', array('proud-actions-app-libraries'), false, true);
-    wp_enqueue_script('proud-actions-app-templates', $path . '/views/app.templates.js', array('proud-actions-app-app'), false, true);
+    wp_enqueue_script('proud-actions-app-libraries', $path . 'js/libraries.min.js', array('angular'), false, true);
+    wp_enqueue_script('proud-actions-app-app', $path . 'js/app.min.js', array('proud-actions-app-libraries'), false, true);
+    wp_enqueue_script('proud-actions-app-templates', $path . 'views/app.templates.js', array('proud-actions-app-app'), false, true);
     
     // @todo: make this work (file isn't getting included with this call, so I just added it to app.min.js)
     //if ($this->settings['active_tabs']['#options']['vote']) {
